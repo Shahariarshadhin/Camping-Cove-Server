635b5d595b5711141e968d6b2f9e8ede53c51155
shahriarshadhin <shadhin19shahriar@gmail.com>
2022-05-01 04:16:33 +0600

    project inialization

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const res = require('express/lib/response');
+const req = require('express/lib/request');
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iyytm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const gearCollection = client.db('campingCove').collection('gear');
+
+        app.get('/gear', async (req, res) => {
+            const query = {};
+            const cursor = gearCollection.find(query);
+            const gears = await cursor.toArray();
+            res.send(gears);
+        })
+    }
+    finally { }
+
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from camping cove')
+});
+
+app.listen(port, () => {
+    console.log('Camping Cove is running on port', port);
+})

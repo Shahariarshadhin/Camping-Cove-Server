e26b89a006ff397d7a3faba8a6d2fc81aedaa1bc
Shahariarshadhin <shadhin19shahriar@gmail.com>
2022-12-24 21:16:05 +0600

    added vercel

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
         const gearCollection = client.db('campingCove').collection('gear');
 
         app.get('/gear', async (req, res) => {
